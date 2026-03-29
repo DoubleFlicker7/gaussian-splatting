1d33d75242b693977b82d64d01643301a83fb8e0
DoubleFlicker7 <eddyxu2000@gmail.com>
2026-03-29 03:53:50 +0000

    [format] 20260329.4

--- docker_startup.sh
+++ docker_startup.sh
@@ -1,7 +1,8 @@
 docker run \
     -it \
+    --name 3dgs_i2_2_c1_0 \
     --gpus all \
-    -v /data/dataset/public:/data/dataset/public:ro \
+    -v /data/dataset/public:/data/dataset/public \
     -v /data/xuhaonan/project/gaussian-splatting/output:/workspace/gaussian-splatting/output \
     3dgs:v2.2 \
     bash
